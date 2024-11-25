2e24da39e3a90a535bafb1eafe28529fb8073280
nathan <nathanlunceford@gmail.com>
2024-11-25 08:40:27 -0700

    Added GitHub link

--- projectVite/src/index/index.jsx
+++ projectVite/src/index/index.jsx
@@ -7,8 +7,19 @@ export const Index = () => {
       <div>
         <h1>Welcome</h1>
         <nav>
+          
+        <div>
+          <span>@</span>
+          <input type="text" placeholder="your@email.com" />
+        </div>
+        <div>
+          <span>🔒</span>
+          <input type="password" placeholder="password" />
+        </div>
+
           <a href="#">🔒 Login</a>
           <a href="#">Create Account</a>
+          <a href="https://github.com/n8lunceford/CS_260_Startup/tree/main">GitHub</a>
         </nav>
       </div>
     );
