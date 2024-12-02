71b64d61ba2b7d308eb4b6acb920dc20b94857fc
nathan <nathanlunceford@gmail.com>
2024-12-02 10:06:56 -0700

    Coded login and create

--- projectVite/src/index/index.jsx
+++ projectVite/src/index/index.jsx
@@ -3,6 +3,24 @@ import React from 'react';
 import './index.css';
 
 export const Index = () => {
+
+  async function loginOrCreate(endpoint) {
+    const response = await fetch(endpoint, {
+      method: 'post',
+      body: JSON.stringify({ email: userName, password: password }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+    if (response?.status === 200) {
+      localStorage.setItem('userName', userName);
+      props.onLogin(userName);
+    } else {
+      const body = await response.json();
+      setDisplayError(`⚠ Error: ${body.msg}`);
+    }
+  }
+
     return (
       <div>
         <h1>Welcome</h1>
@@ -17,8 +35,8 @@ export const Index = () => {
           <input type="password" placeholder="password" />
         </div>
 
-          <a href="#">🔒 Login</a>
-          <a href="#">Create Account</a>
+          <a href="#" onClick={() => loginOrCreate('/api/auth/login')}>🔒 Login</a>
+          <a href="#"onClick={() => loginOrCreate('/api/auth/create')}>Create Account</a>
           <a href="https://github.com/n8lunceford/CS_260_Startup/tree/main">GitHub</a>
         </nav>
       </div>
